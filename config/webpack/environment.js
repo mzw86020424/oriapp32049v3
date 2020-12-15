8edffd3d06ced773f49952aa8f870f149b6eeee4
mzw86020424 <mzw86020424@gmail.com>
2020-12-16 01:04:02 +0900

    Revert "WebpackerでjQueryとBootstrapとpopper.jsを追加"
    This reverts commit 489b375057347c2212043fbc6f1a206db479a43f.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,21 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-
-// Vue.js フル版（Compiler入り）
-environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }
-
-// ここから
-// jQueryとBootstapのJSを使えるように
-const webpack = require('webpack')
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: 'popper.js'
-  })
-)
-// ここまで
 module.exports = environment
